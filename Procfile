63ff1bf57c270dbad6ae3d4d102c32d8eb7dfd30
Jason Benn <jasoncbenn@gmail.com>
2016-12-10 20:03:35 -0800

    First commit.

---
+++ Procfile
@@ -0,0 +1,1 @@
+serve: python -m SimpleHTTPServer
---
+++ index.js
@@ -0,0 +1,7 @@
+// $(window).ready(function() {
+//   console.log("ready")
+//   setTimeout(function() {
+//     console.log("reloading")
+//     window.location.reload()
+//   }, 1000);
+// })
