abfba819c9cd51f1c6c40a0a93455352b027a551
Jason Benn <jasoncbenn@gmail.com>
2017-01-27 01:27:40 -0800

    Almost done.

--- files/html/element.tpl
+++ files/html/element.tpl
@@ -1,5 +1,1 @@
-<!-- <link rel="stylesheet" href="./index.css"> -->
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css" integrity="sha256-uHu2MAd1LvCOVEAhvMld4LpJi7dUGS7GVzvG/5B3hlo=" crossorigin="anonymous" /> -->
-<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity crossorigin="anonymous" /> -->
-<!-- Will need to convert ^ font awesome SVG into files, reference them like so: background-image: url("@{assets_path}file.png"); -->
 <div class="nested-accordion"></div>
--- files/js/element.js
+++ files/js/element.js
@@ -62,7 +62,7 @@
       return "\
       <div class='folder' data-structure={{structure}}>\
         <section class='folder-label nesting-{{nesting}}'>\
-          <img src='"+this.assets_path+"angle-right.svg' class='folder-arrow'>\
+          <svg class='folder-arrow' fill='#fff' viewBox='200 300 1300 1300' xmlns='http://www.w3.org/2000/svg'><path d='M1171 960q0 13-10 23l-466 466q-10 10-23 10t-23-10l-50-50q-10-10-10-23t10-23l393-393-393-393q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l466 466q10 10 10 23z'/></svg>\
           {{label}}\
         </section>\
         <div class='children' style='display: none;'>\
@@ -95,10 +95,7 @@
     },
 
     openToCurrentUrl: function(accordion) {
-      // TODO: use location.pathname
-      var $currentLink = $accordion.find("[href='" + "/identifying-partners" + "']");
-      $currentLink.closest('section').addClass('current');
-      var $currentLink = $accordion.find("[href='" + "/effectiveness" + "']");
+      var $currentLink = $accordion.find("[href='" + location.pathname + "']");
       $currentLink.closest('section').addClass('current');
       $currentLink.parentsUntil('.nested-accordion', '.folder').addClass('opened');
       $currentLink.parentsUntil('.nested-accordion', '.children').toggle();
